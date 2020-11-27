566281333f760d11ca9bd9fac3c9f57f1e26e227
Poul Henriksen <pohe@easj.dk>
2020-11-27 11:18:00 +0100

    Added Personcatalog

---
+++ GitTest2020/PersonCatalog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest2020
+{
+    class PersonCatalog
+    {
+        private List<Person> persons;
+
+        public PersonCatalog()
+        {
+            persons = new List<Person>();
+        }
+    }
+}
