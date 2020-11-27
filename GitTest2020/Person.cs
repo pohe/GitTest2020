028525208f6e863c8508a05379fb56e821b0d34c
Poul Henriksen <pohe@easj.dk>
2020-11-27 10:37:29 +0100

    Added Person class

---
+++ GitTest2020/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest2020
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
