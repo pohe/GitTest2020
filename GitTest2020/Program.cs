5d1d85d46766172312c2488ea233b1dfbc649d94
Poul Henriksen <pohe@easj.dk>
2020-11-27 10:33:14 +0100

    Add project files.

---
+++ GitTest2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
